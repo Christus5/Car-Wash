457b607211f0f40b9a110df6d5aae3b4e3b422dc
Luka Tavkhelidze <52665503+Christus5@users.noreply.github.com>
2021-01-23 03:50:45 +0400

    create models

--- washapp/admin.py
+++ washapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import (Employee, Permission, Role)
+
+
 # Register your models here.
+@admin.register(Employee)
+class EmployeeAdminModel(admin.ModelAdmin):
+    readonly_fields = ['created']
+
+admin.site.register(Permission)
+admin.site.register(Role)
--- washapp/models.py
+++ washapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
+class Employee(models.Model):
+    full_name = models.CharField(max_length=100)
+
+    role = models.ForeignKey(to='washapp.Role', on_delete=models.PROTECT)
+    hired_date = models.DateField('Date Hired')
+
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Added to DB')
+
+    def __str__(self):
+        return self.full_name
+
+
+class Role(models.Model):
+    title = models.CharField(max_length=50)
+    salary = models.PositiveSmallIntegerField(verbose_name='Salary / Monthly')
+
+    permissions = models.ManyToManyField(to='washapp.Permission')
+
+    class Meta:
+        verbose_name = 'Job'
+        verbose_name_plural = 'Jobs'
+
+    def __str__(self):
+        return self.title
+
+
+class Permission(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
