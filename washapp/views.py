4de453b026aeaeb05a15a0b21159982cb42952d4
Luka Tavkhelidze <52665503+Christus5@users.noreply.github.com>
2021-01-23 04:16:30 +0400

    update view

--- washapp/views.py
+++ washapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from .models import (CarWashBooth, Car)
+
 
 # Create your views here.
 def index_view(request, *args, **kwargs):
-    return render(request, 'washapp/index.html')
+
+    context = {
+        'booths': CarWashBooth.objects.filter(occupied=False),
+        'cars': Car.objects.all()
+    }
+    return render(request, 'washapp/index.html', context)
