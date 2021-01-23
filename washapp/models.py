87252414876d6394f588b0845e40fd971f22480f
Luka Tavkhelidze <52665503+Christus5@users.noreply.github.com>
2021-01-23 04:20:23 +0400

    fix models

--- washapp/models.py
+++ washapp/models.py
@@ -38,8 +38,8 @@ class Permission(models.Model):
 
 class CarWashBooth(models.Model):
     occupied = models.BooleanField(blank=True)
-    occupant = models.ForeignKey(to='washapp.Employee', on_delete=models.PROTECT, null=True, blank=True)
-    car = models.ForeignKey(to='washapp.Car', on_delete=models.PROTECT, null=True, blank=True)
+    occupant = models.ForeignKey(to='washapp.Employee', on_delete=models.PROTECT, null=True, blank=True, unique=True)
+    car = models.ForeignKey(to='washapp.Car', on_delete=models.PROTECT, null=True, blank=True, unique=True)
 
     def __str__(self):
         return str(self.id)
