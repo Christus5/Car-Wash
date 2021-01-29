5a364ae28821c3924d77a2758e9c5f4c5385c07a
Luka Tavkhelidze <52665503+Christus5@users.noreply.github.com>
2021-01-29 18:08:50 +0400

    Update models

--- washapp/admin.py
+++ washapp/admin.py
@@ -1,15 +1,36 @@
 from django.contrib import admin
 
 from .models import Employee
-from .models import Permission
-from .models import Role
 from .models import Car
 from .models import CarWashBooth
+from .models import Order
+from .models import Company
+from .models import BoothToCompany
+from .models import EmployeeToCompany
+from .models import OrderToCompany
 
 
 # Register your models here.
+class BoothToCompanyInline(admin.TabularInline):
+    model = BoothToCompany
+    extra = 0
+
+class EmployeeToCompanyInline(admin.TabularInline):
+    model = EmployeeToCompany
+    extra = 0
+
+
+class OrderToCompanyInline(admin.TabularInline):
+    model = OrderToCompany
+    extra = 0
+
 @admin.register(Employee)
 class EmployeeAdminModel(admin.ModelAdmin):
     readonly_fields = ['created']
 
-admin.site.register([Permission, Role, Car, CarWashBooth])
+
+@admin.register(Company)
+class CompanyAdminModel(admin.ModelAdmin):
+    inlines = [BoothToCompanyInline, EmployeeToCompanyInline, OrderToCompanyInline]
+
+admin.site.register([Car, CarWashBooth, Order])
--- washapp/models.py
+++ washapp/models.py
@@ -2,38 +2,53 @@ from django.db import models
 
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    booths = models.ManyToManyField(to='washapp.CarWashBooth', related_name='booths', through='washapp.BoothToCompany')
+    employees = models.ManyToManyField(to='washapp.Employee', through='washapp.EmployeeToCompany')
+    orders = models.ManyToManyField(to='washapp.Order', through='washapp.OrderToCompany')
+
+    class Meta:
+        verbose_name_plural = "Companies"
+
+    def __str__(self):
+        return self.name
+
+
+class BoothToCompany(models.Model):
+    company = models.ForeignKey(to='washapp.Company', on_delete=models.PROTECT)
+    booth = models.OneToOneField(to='washapp.CarWashBooth', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Booth"
+        verbose_name_plural = "Booths"
+
+
+class EmployeeToCompany(models.Model):
+    employee = models.OneToOneField(to='washapp.Employee', on_delete=models.CASCADE)
+    company = models.ForeignKey(to='washapp.Company', on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = 'Employee'
+        verbose_name_plural = 'Employees'
+
+
+class OrderToCompany(models.Model):
+    order = models.OneToOneField(to='washapp.Order', on_delete=models.CASCADE)
+    company = models.ForeignKey(to='washapp.Company', on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
+
+
 class Employee(models.Model):
     full_name = models.CharField(max_length=100)
 
-    role = models.OneToOneField(to='washapp.Role', on_delete=models.PROTECT)
-    hired_date = models.DateField('Date Hired')
-
-    created = models.DateTimeField(auto_now_add=True, verbose_name='Added to DB')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Hired')
 
     def __str__(self):
         return self.full_name
-
-
-class Role(models.Model):
-    title = models.CharField(max_length=50)
-    salary = models.PositiveSmallIntegerField(verbose_name='Salary / Monthly')
-
-    permissions = models.ManyToManyField(to='washapp.Permission')
-
-    class Meta:
-        verbose_name = 'Job'
-        verbose_name_plural = 'Jobs'
-
-    def __str__(self):
-        return self.title
-
-
-class Permission(models.Model):
-    title = models.CharField(max_length=20)
-    description = models.TextField(null=True, blank=True)
-
-    def __str__(self):
-        return self.title
 
 
 class CarWashBooth(models.Model):
@@ -50,3 +65,18 @@ class Car(models.Model):
 
     def __str__(self):
         return self.license_plate
+
+
+class Order(models.Model):
+    car = models.ForeignKey(to='washapp.Car', on_delete=models.PROTECT)
+    wash_booth = models.ForeignKey(to='washapp.CarWashBooth', on_delete=models.PROTECT)
+    washer = models.ForeignKey(to='washapp.Employee', on_delete=models.CASCADE)
+    price = models.PositiveSmallIntegerField()
+    requested = models.DateTimeField()
+    completed = models.DateTimeField()
+
+    class Meta:
+        unique_together = [['washer', 'wash_booth', 'requested', 'car']]
+
+    def __str__(self):
+        return str(self.pk)
