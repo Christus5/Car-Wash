fceaa1faaeddcd8bb92a9fa32bdbc8d86f94d6ef
Luka Tavkhelidze <52665503+Christus5@users.noreply.github.com>
2021-02-05 18:41:35 +0400

    add search

--- washapp/views.py
+++ washapp/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 
@@ -10,10 +11,15 @@ from .models import Employee
 
 # Create your views here.
 def index_view(request, *args, **kwargs):
+    search = request.GET.get('name')
+    employee_q = Q()
+    if search:
+        employee_q &= Q(name__icontains=search[-1]) | Q(last_name__icontains=search[-1])
 
     context = {
-        'employees': Employee.objects.all()
+        'employees': Employee.objects.filter(employee_q)
     }
+
     return render(request, 'washapp/index.html', context)
 
 def detail_view(request, employee_id):
