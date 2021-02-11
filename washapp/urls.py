47faf20b0656ac42361d70869c8981604461e6ba
Luka Tavkhelidze <52665503+Christus5@users.noreply.github.com>
2021-02-11 12:38:36 +0400

    update emloyees view

--- washapp/urls.py
+++ washapp/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
-from .views import index_view
-from .views import detail_view
-from .views import cars_view
-
+from .views import *
 
 urlpatterns = [
     path('', index_view, name='index'),
     path('employee/<int:employee_id>/', detail_view, name='detail'),
-    path('cars/<int:page>/', cars_view,name='cars')
+    path('cars/<int:page>/', cars_view, name='cars'),
+    path('employees/<int:page>/', employees_view, name='employees')
 ]
--- washapp/views.py
+++ washapp/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.template.defaulttags import register
 
-
 from .models import CarWashBooth
 from .models import Car
 from .models import Order
@@ -27,13 +26,7 @@ def index_view(request, *args, **kwargs):
         if orderForm.is_valid():
             orderForm.save()
 
-    search = request.GET.get('name')
-    employee_q = Q()
-    if search:
-        employee_q &= Q(name__icontains=search) | Q(last_name__icontains=search)
-
     context = {
-        'employees': Employee.objects.filter(employee_q),
         'order_form': orderForm,
         'car_count': Car.objects.count(),
         'employee_count': Employee.objects.count()
@@ -41,11 +34,24 @@ def index_view(request, *args, **kwargs):
 
     return render(request, 'washapp/index.html', context)
 
+
 def cars_view(request, page):
     return render(request, 'washapp/cars.html', {
         'cars': Car.objects.all()[page * 5:(5 * page) + 5],
         'current_page': page,
         'pages': int(Car.objects.count() / 5)
+    })
+
+
+def employees_view(request, page):
+    search = request.GET.get('name')
+    employee_q = Q()
+    if search:
+        employee_q &= Q(name__icontains=search) | Q(last_name__icontains=search)
+
+    return render(request, 'washapp/employess.html', {
+        'employees': Employee.objects.filter(employee_q)[page * 5:(page * 5) + 5],
+        'current_page': page
     })
 
 
