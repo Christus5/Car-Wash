0994f7c0e288f287227bec087911538aa6602f3d
Luka Tavkhelidze <52665503+Christus5@users.noreply.github.com>
2021-02-11 19:27:08 +0400

    update form visual and form sturcture

--- washapp/forms.py
+++ washapp/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 
-from .models import Order
+from .models import *
 
 
 class OrderForm(forms.ModelForm):
+    requested = forms.DateTimeField(widget=forms.DateInput(attrs={
+        'type': 'date'
+    }))
+    completed = forms.DateTimeField(widget=forms.DateInput(attrs={
+        'type': 'date'
+    }))
+
+    car = forms.ModelChoiceField(empty_label='Select Car', queryset=Car.objects.all())
+    wash_booth = forms.ModelChoiceField(empty_label='Select Booth', queryset=CarWashBooth.objects.all())
+    washer = forms.ModelChoiceField(empty_label='Select Washer', queryset=Employee.objects.all())
+    price = forms.DecimalField(min_value=0)
+
     class Meta:
         model = Order
         fields = '__all__'
